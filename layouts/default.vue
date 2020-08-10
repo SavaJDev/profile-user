a54b4ee949da8dbdb5e25a76b8404e81185634ee
SavaJDev <40608391+SavaJDev@users.noreply.github.com>
2020-08-10 10:20:00 +0300

    Add files via upload

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-app app>
+            <v-app-bar app> 
+                <h2>Profile page</h2>
+            </v-app-bar>
+            <v-main>
+                <v-container fluid>
+                    <nuxt/>
+                </v-container>
+            </v-main>
+    </v-app>
+</template>
+
+<script>
+    
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+
+</script>
